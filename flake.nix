931b4ce86cadd39d61c801a59c4acf7aba678ce3
Seija <seijamail@duck.com>
2026-01-10 12:04:55 -0500

    change formatter for nix

--- flake.nix
+++ flake.nix
@@ -9,30 +9,32 @@
     zls.inputs.zig-overlay.follows = "zig";
   };
 
-  outputs = {
-    nixpkgs,
-    zig,
-    zls,
-    ...
-  }: let
-    forAllSystems = f:
-      builtins.mapAttrs
-      f
-      nixpkgs.legacyPackages;
-  in {
-    formatter = forAllSystems (system: pkgs: pkgs.alejandra);
+  outputs =
+    {
+      nixpkgs,
+      zig,
+      zls,
+      ...
+    }:
+    let
+      forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+    in
+    {
+      formatter = forAllSystems (system: pkgs: pkgs.nixfmt-rfc-style);
 
-    devShells = forAllSystems (system: pkgs: {
-      default = pkgs.mkShellNoCC {
-        packages = [
-          (pkgs.writeShellScriptBin "serve" ''
-            ${pkgs.lib.getExe pkgs.caddy} file-server --root ./zig-out/docs/ --listen :8000
-          '')
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              (pkgs.writeShellScriptBin "serve" ''
+                ${pkgs.lib.getExe pkgs.caddy} file-server --root ./zig-out/docs/ --listen :8080
+              '')
 
-          zig.packages.${system}.nightly
-          zls.packages.${system}.zls
-        ];
-      };
-    });
-  };
+              zig.packages.${system}.nightly
+              zls.packages.${system}.zls
+            ];
+          };
+        }
+      );
+    };
 }
