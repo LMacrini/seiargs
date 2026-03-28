5262b2e307a74273ed1b9c6ca77501a33653a895
Seija <seijamail@duck.com>
2026-03-28 11:38:06 -0400

    update nix stuff

--- flake.nix
+++ flake.nix
@@ -1,29 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    zig.url = "github:silversquirl/zig-flake/compat";
-    zls.url = "github:zigtools/zls";
-
+    zig.url = "github:silversquirl/zig-flake";
     zig.inputs.nixpkgs.follows = "nixpkgs";
-    zls.inputs.nixpkgs.follows = "nixpkgs";
-    zls.inputs.zig-overlay.follows = "zig";
   };
 
   outputs =
     {
       nixpkgs,
       zig,
-      zls,
       ...
     }:
     let
-      forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
+      forAllSystems =
+        f: builtins.mapAttrs (system: pkgs: f pkgs zig.packages.${system}.nightly) nixpkgs.legacyPackages;
     in
     {
-      formatter = forAllSystems (system: pkgs: pkgs.nixfmt-rfc-style);
+      formatter = forAllSystems (pkgs: zig: pkgs.nixfmt-rfc-style);
 
       devShells = forAllSystems (
-        system: pkgs: {
+        pkgs: zig: {
           default = pkgs.mkShellNoCC {
             name = "seiargs";
 
@@ -34,27 +30,27 @@
                 ${pkgs.lib.getExe pkgs.caddy} file-server --root ./zig-out/docs/ --listen :8080
               '')
 
-              zig.packages.${system}.nightly
-              zls.packages.${system}.zls
+              zig
+              zig.zls
             ];
           };
 
           minimal = pkgs.mkShellNoCC {
             packages = [
-              zig.packages.${system}.nightly
+              zig
             ];
           };
         }
       );
 
       packages = forAllSystems (
-        system: pkgs: {
+        pkgs: zig: {
           docs = pkgs.stdenvNoCC.mkDerivation {
             name = "seiargs-docs";
             src = ./.;
 
             nativeBuildInputs = [
-              zig.packages.${system}.nightly
+              zig
             ];
 
             buildPhase = ''
